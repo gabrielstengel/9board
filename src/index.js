0a41e6aa4408803d5cbdb3069fe4080688ea387f
gabrielstengel <gstengel@princeton.edu>
2018-06-08 11:04:24 -0400

    Finishing vanilla tic-tac-toe, starting to implement 9-board.

--- src/index.js
+++ src/index.js
@@ -2,31 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square" onClick={() => this.props.onClick()}>
-        {this.props.value}
-      </button>
-    );
-  }
+// SQUARE:
+// Functional component for rendering individual squares in a tic-tac-toe game
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
 
+// BOARD:
 class Board extends React.Component {
-
 // Board Constructor with 9 empty values corresponding to squares
   constructor(props) {
       super(props);
       this.state = {
         squares: Array(9).fill(null),
+        xIsNext: true,
       };
     }
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
+
+    // Do nothing if game is done.
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+
+    // Otherwise handle normally
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   renderSquare(i) {
@@ -37,7 +47,12 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const winner = calculateWinner(this.state.squares);
+    let status;
+
+    // Set status based on whether or not game is over
+    if (winner) { status = 'Winner: ' + winner; }
+    else { status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');}
 
     return (
       <div>
@@ -67,7 +82,7 @@ class Game extends React.Component {
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board /> <Board /> <Board />
         </div>
         <div className="game-info">
           <div>{/* status */}</div>
@@ -76,6 +91,27 @@ class Game extends React.Component {
       </div>
     );
   }
+}
+
+// Helper function for checking if an individual board has been won
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 // ========================================
